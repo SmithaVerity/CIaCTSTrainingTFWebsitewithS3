e0740b735b5f8404bbabc6a0d8501a4ab04f931f
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2024-02-29 20:59:50 +0530

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -95,3 +95,13 @@ resource "aws_security_group_rule" "egress" {
 
   cidr_blocks = ["0.0.0.0/0"]
 }
+
+resource "aws_security_group_rule" "ingress" {
+  from_port         = 22
+  protocol          = "tcp"
+  security_group_id = aws_security_group.this.id
+  to_port           = 22
+  type              = "ingress"
+
+  cidr_blocks = ["0.0.0.0/0"]
+}
