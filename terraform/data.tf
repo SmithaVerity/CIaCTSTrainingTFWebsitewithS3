32ff06e7a8782890d78d2820914eb9b6f801e73a
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2024-02-29 19:56:37 +0530

    Update data.tf

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,18 +1,3 @@
-data "aws_ami" "this" {
-  most_recent = true
-
-
-  filter {
-    name   = "owner-alias"
-    values = ["amazon"]
-  }
-
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm*"]
-  }
-}
 
 data "aws_availability_zones" "this" {}
 
