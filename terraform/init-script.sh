0e103cc1f82f6613a04ee382c2507e005083e025
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2024-03-01 15:47:45 +0530

    Update init-script.sh

--- terraform/init-script.sh
+++ terraform/init-script.sh
@@ -7,12 +7,14 @@ set -e
 sudo apt-get update
 sudo apt-get install -y apache2
 sudo systemctl enable apache2
-sudo wget https://s3.amazonaws.com/mountpoint-s3-release/latest/x86_64/mount-s3.deb
-sudo apt-get install ./mount-s3.deb -y
-sudo rm -f ./mount-s3.deb
-sudo mkdir /mount_s3
-sudo mount-s3 ${module.s3.s3_bucket_id} /mount_s3
+sudo apt-get install awscli -y
+sudo apt-get install s3fs -y
 git clone https://github.com/SmithaVerity/ABTestingApp.git
 mv ABTestingApp/cafe /var/www/html
+sudo mkdir /home/ubuntu/bucket
+cd $HOME/bucket
+sudo touch test1.txt test2.txt test3.txt
+aws s3 sync /home/ubuntu/bucket s3://${module.s3.s3_bucket_id}
+sudo s3fs ${module.s3.s3_bucket_id} /home/ubuntu/bucket -o iam_role=${var.environment}_ec2_role -o use_cache=/tmp -o allow_other -o uid=1001 -o mp_umask=002 -o multireq_max=5 -o use_path_request_style -o url=https://s3-ap-south-1.amazonaws.com -o dbglevel=info -f -o curldbg
 
 
