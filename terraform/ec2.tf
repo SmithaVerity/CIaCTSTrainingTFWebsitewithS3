848d4c1dc9fa70b436d2b43fb20f93f10baae6dc
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2024-03-01 13:53:18 +0530

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -57,6 +57,20 @@ resource "aws_instance" "this" {
   }
   user_data = "${file("init-script.sh")}"
 
+  user_data = <<-EOF
+      #!/bin/bash
+      sudo apt-get update
+      sudo apt-get install -y apache2
+      sudo systemctl enable apache2
+      sudo wget https://s3.amazonaws.com/mountpoint-s3-release/latest/x86_64/mount-s3.deb
+      sudo apt-get install ./mount-s3.deb -y
+      sudo rm -f ./mount-s3.deb
+      sudo mkdir /mount_s3
+      sudo mount-s3 ${module.s3.s3_bucket_id} /mount_s3
+      git clone https://github.com/SmithaVerity/ABTestingApp.git
+      mv ABTestingApp/cafe /var/www/html
+    EOF
+
   lifecycle {
     ignore_changes = [user_data, ami, vpc_security_group_ids]
   }
