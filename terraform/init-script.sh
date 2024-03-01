226a731bf6a88b14303dba099f9dfe1616549045
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2024-03-01 13:38:13 +0530

    Update init-script.sh

--- terraform/init-script.sh
+++ terraform/init-script.sh
@@ -3,17 +3,16 @@
 set -e
 
 # Update packages on the system
-sudo -s
-apt-get update
-apt-get install -y apache2
-systemctl enable apache2
-wget https://s3.amazonaws.com/mountpoint-s3-release/latest/x86_64/mount-s3.deb
-apt install ./mount-s3.deb -y
-rm -f ./mount-s3.deb
-mkdir /mount_s3
-mount-s3 ${module.s3.s3_bucket_id} /mount_s3
 
-  git clone https://github.com/SmithaVerity/ABTestingApp.git
+sudo apt-get update
+sudo apt-get install -y apache2
+sudo systemctl enable apache2
+sudo wget https://s3.amazonaws.com/mountpoint-s3-release/latest/x86_64/mount-s3.deb
+sudo apt-get install ./mount-s3.deb -y
+sudo rm -f ./mount-s3.deb
+sudo mkdir /mount_s3
+sudo mount-s3 ${module.s3.s3_bucket_id} /mount_s3
+git clone https://github.com/SmithaVerity/ABTestingApp.git
+mv ABTestingApp/cafe /var/www/html
 
-  mv ABTestingApp/cafe /var/www/html
 
