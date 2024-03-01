7b5d5507d0b391f000a98bd0fd9608bed53bacad
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2024-03-01 20:05:45 +0530

    Update init-script.sh

--- terraform/init-script.sh
+++ terraform/init-script.sh
@@ -14,7 +14,5 @@ mv ABTestingApp/cafe /var/www/html
 sudo mkdir /home/ubuntu/bucket
 cd /home/ubuntu/bucket
 sudo touch test1.txt test2.txt test3.txt
-aws s3 sync /home/ubuntu/bucket s3://${module.s3.s3_bucket_id}
-sudo s3fs ${module.s3.s3_bucket_id} /home/ubuntu/bucket -o iam_role=${var.environment}_ec2_role -o use_cache=/tmp -o nonempty -o allow_other -o uid=1001 -o mp_umask=002 -o multireq_max=5 -o use_path_request_style -o url=https://s3-ap-south-1.amazonaws.com -o dbglevel=info -f -o curldbg
 
 
